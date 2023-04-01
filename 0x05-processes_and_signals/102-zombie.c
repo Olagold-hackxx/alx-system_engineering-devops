e573f18cbb14219a5083a144c48eece9e12d49f4
Abdulqoyyum Aileru <Abdulqoyyum Aileru>
2023-04-01 17:40:06 -0400

    Solve advance tasks

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,31 +9,32 @@
 
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
 * main - main function
-* Return - exit
+* Return: exit
 */
 
 int main(void)
 {
-    int i;
-    int x;/*Create child process*/
-    for (i = 0; i < 5; i++)
-    {
-        x = fork();
-        if (x == 0)
-        {
-            printf("Zombie process created, PID: %d %d\n", getpid(), i);
-            exit(0);
-        }
-    }
-    infinite_while();
-    return (0);
+	int i;
+	int x;/*Create child process*/
+
+	for (i = 0; i < 5; i++)
+	{
+		x = fork();
+		if (x == 0)
+		{
+			printf("Zombie process created, PID: %d %d\n", getpid(), i);
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
 }
