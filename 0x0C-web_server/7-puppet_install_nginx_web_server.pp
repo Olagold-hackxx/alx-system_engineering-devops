c791fc37b3bf5f3d9ed0914f6aad794c0b4f4d12
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-06-28 10:07:01 -0400

    Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,6 @@
 # Install and configure nginx
-package { 'jfryman-nginx':
-  ensure => installed,
-}
-
-include nginx
-
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-stable',
-}
-
-nginx::resource::server { '34.73.76.135':
-  listen_port      => 80,
-  www_root         => '/var/www/html/',
-  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
-}
-
-file { 'index':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World!',
+exec { 'install_nginx':
+    provider => 'shell',
+    command => 'apt-get update -y; apt-get install nginx -y; ufw allow 'Nginx Https'; echo "Hello World!" > /var/www/html/index.html;
+    echo "Ceci n'est pas une page" > /var/www/html/error404.html; sed -i '/server_name _/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-enabled/default; sed -i '/rewrite/a  error_page 404 /error404.html;' /etc/nginx/sites-enabled/default; service nginx restart'
 }
