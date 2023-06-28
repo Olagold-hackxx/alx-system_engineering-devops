92c038c47f8d1d44790631b923f1d97581d457b4
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-06-27 22:13:42 -0400

    Task 4

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,15 @@
+class{'nginx':
+    manage_repo => true,
+    package_source => 'nginx-mainline'
+}
+nginx::resource::location {'server-location':
+    location_cfg_append => {
+        'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+    }
+}
+file { '/var/www/html/index.html':
+    content => 'Hello World!'
+}
+file { '/var/www/html/error404.html':
+    content => "Ceci n'est pas une page"
+}
