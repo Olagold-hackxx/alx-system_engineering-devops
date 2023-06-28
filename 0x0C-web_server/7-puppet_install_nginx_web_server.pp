030269bcc52ad6785d3d7d57c6f6f5135ea05461
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-06-27 23:02:56 -0400

    Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,20 @@
-# Install and configure nginx
-package { 'jfryman-nginx':
-  ensure => installed,
+package {'jfryman-nginx':
+    ensure => 'installed',
 }
-
 include nginx
 
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-stable',
+class{'nginx':
+    manage_repo => true,
+    package_source => 'nginx-mainline'
 }
-
-nginx::resource::server { '34.73.76.135':
-  listen_port      => 80,
-  www_root         => '/var/www/html/',
-  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
+nginx::resource::vhost {'default':
+    location_cfg_append => {
+        'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+    }
 }
-
-file { 'index':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Holberton School for the win!',
+file { '/var/www/html/index.html':
+    content => 'Hello World!'
+}
+file { '/var/www/html/error404.html':
+    content => "Ceci n'est pas une page"
 }
