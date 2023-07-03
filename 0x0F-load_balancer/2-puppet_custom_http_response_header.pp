75129561000050a16112e8669e99100915dd72f1
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-03 18:26:45 -0400

    Automate with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Add a custom header to response
 exec { 'Add_header':
     provider => 'shell',
-    command  => "sed -i '/^\slocation /a \t\tadd_header X-Served-By $hostname;' /etc/nginx/sites-enabled/default;
-    sudo service nginx restart"
+    command  => 'sudo sed -i "/^\slocation /a \\\\\t\tadd_header X-Served-By \$hostname;" /etc/nginx/sites-enabled/default;
+    sudo service nginx restart'
 }
