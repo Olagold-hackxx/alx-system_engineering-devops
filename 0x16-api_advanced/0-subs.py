f947881d8f7b56e18ec3ac78f2b279c69410f3e7
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-08-10 20:58:32 -0400

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" A function that queries the Reddit API and returns
+the number of subscribers
+(not active users, total subscribers) for a given subreddit."""
 
 
 def number_of_subscribers(subreddit):
+    """Query api"""
     import requests
     import json
 
